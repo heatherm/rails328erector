8f88469d97a946c38a30f5d5c3a8d0a91dd3bf63
Heather Moore <moore.hj@gmail.com>
2012-11-18 12:13:01 -0800

    add new project with erector

---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+require 'erector'
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Rails328erector::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails328erector::Application.config.secret_token = 'b01dba94ed1417f7d075234729bd875e6c7ae285a534f297955122f2113eb0faeb4a7d32180a6624e9e7e1cbc0ef39fc677952cbad9e6f06d7e3a13e1863e198'
